c2abaa9896aa0e23779fa98f815ea9e9e7637824
dg12kumbhar <dg12kumbhar@gmail.com>
2020-09-12 18:00:23 +0530

    Header and login page change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
