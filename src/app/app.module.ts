61911436643aff5351b523b35856e2832c4bd609
dg12kumbhar <dg12kumbhar@gmail.com>
2020-09-13 17:15:53 +0530

    Add post component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,25 @@ import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddPostComponent } from './add-post/add-post.component';
+import { EditorModule } from '@tinymce/tinymce-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    EditorModule,
     RouterModule.forRoot([
-      { path: 'login', component: LoginComponent }
+      { path: 'login', component: LoginComponent },
+      { path: 'add-post', component: AddPostComponent }
     ]),
   ],
   providers: [],
